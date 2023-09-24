f481d1b5ba7c21bcd1814fefecafcfc842b1188e
prasanth149 <144668872+prasanth149@users.noreply.github.com>
2023-09-24 21:16:31 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,57 +1,17 @@
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0):
-        self._account_number = 4522128030
-        self._account_holder_name = 'prasanth'
-        self._account_balance = 5001
+def linear_search_product(product_list, target_product):
+    indices = []
+    for index, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(index)
+    return indices
 
-    def deposit(self, amount):
-        if amount > 0:
-            self._account_balance += amount
-            print(f"Deposited {amount}. New balance: {self._account_balance}")
-        else:
-            print("Invalid deposit amount. Amount must be greater than 0.")
+# Test the function with a list of products
+products = ["Apple", "Banana", "Orange", "Apple", "Grapes", "Apple"]
+target_product = "Apple"
 
-    def withdraw(self, amount):
-        if 0 < amount <= self._account_balance:
-            self._account_balance -= amount
-            print(f"Withdrew {amount}. New balance: {self._account_balance}")
-        else:
-            print("Invalid withdrawal amount or insufficient balance.")
+result = linear_search_product(products, target_product)
 
-    def display_balance(self):
-        print(f"Account balance for {self._account_holder_name}: {self._account_balance}")
-
-
-if __name__ == "__main__":
-    account_number = 4522128030
-    account_holder_name ='PRASANTH'
-    initial_balance = 5001
-
-    # Creating a new bank account instance
-    my_account = BankAccount(account_number, account_holder_name, initial_balance)
-    def bankaccound(self):
-      print(f" BankAccount {self._account_number}:{self._account_holder_name}:{self._initial_balance}")
-
-    while True:
-        print(f"\n")
-        print(f"BankAccount",(account_number, account_holder_name, initial_balance))
-        print("\nChoose an option:")
-        print("1. Deposit")
-        print("2. Withdraw")
-        print("3. Display Balance")
-        print("4. Exit")
-
-        choice = input("Enter your choice (1/2/3/4): ")
-
-        if choice == "1":
-            deposit_amount = float(input("Enter the deposit amount: "))
-            my_account.deposit(deposit_amount)
-        elif choice == "2":
-            withdraw_amount = float(input("Enter the withdrawal amount: "))
-            my_account.withdraw(withdraw_amount)
-        elif choice == "3":
-            my_account.display_balance()
-        elif choice == "4":
-            break
-        else:
-            print("Invalid choice. Please select a valid option.")
+if result:
+    print(f"The target product '{target_product}' was found at indices: {result}")
+else:
+    print(f"The target product '{target_product}' was not found in the list.")
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
+# Test the function with a list of student objects
+student1 = Student("Alice", "A123", 3.9)
+student2 = Student("Bob", "B456", 3.7)
+student3 = Student("Charlie", "C789", 4.0)
 
-# Creating instances of the derived classes
-player = Player()
-batsman = Batsman()
-bowler = Bowler()
+students = [student1, student2, student3]
 
-# Calling the play() method for each object
-player.play()
-batsman.play()
-bowler.play()
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
